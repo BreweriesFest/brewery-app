263c8be1ec16afb56e7835ba769aa11070eb8ed1
Shubham Goel <shubhamgoyal23@yahoo.com>
2022-12-19 10:19:39 +0530

    fix null pointer for headers

--- common/src/main/java/com/brewery/app/util/Helper.java
+++ common/src/main/java/com/brewery/app/util/Helper.java
@@ -4,14 +4,15 @@ import lombok.experimental.UtilityClass;
 import org.springframework.graphql.server.WebGraphQlRequest;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 @UtilityClass
 public class Helper {
 
-    public static final BiFunction<WebGraphQlRequest, String, String> getHeader = ((request,
-            header) -> collectionAsStream(request.getHeaders().get(header)).findFirst().orElse(null));
+    public static final BiFunction<WebGraphQlRequest, String, Optional<String>> getHeader = ((request,
+            header) -> collectionAsStream(request.getHeaders().get(header)).findFirst());
 
     public static final <T> Stream<T> collectionAsStream(Collection<T> collection) {
         return collection == null ? Stream.empty() : collection.stream();
