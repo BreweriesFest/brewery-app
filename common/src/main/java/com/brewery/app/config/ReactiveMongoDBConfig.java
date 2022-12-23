faf155e4598a2062a15f0ca03abf211d6ed8fcb6
Shubham Goel <shubhamgoyal23@yahoo.com>
2022-12-23 10:42:18 +0530

    refine configuration (#35)
    * add custom health indicator, add customizer for mongo config, get default webClientBuilder , log mongo queries, show git details in info endpoint
    
    * enable mongo configuration based on profile, disable redis auto-configure

--- common/src/main/java/com/brewery/app/config/ReactiveMongoDBConfig.java
+++ common/src/main/java/com/brewery/app/config/ReactiveMongoDBConfig.java
@@ -1,14 +1,19 @@
 package com.brewery.app.config;
 
+import com.mongodb.WriteConcern;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
 import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.reactive.TransactionalOperator;
 
 @Configuration
-public class ReactiveMongoDBTransactionConfig {
+@Profile("mongo-db")
+public class ReactiveMongoDBConfig {
 
     @Bean
     public ReactiveTransactionManager transactionManager(ReactiveMongoDatabaseFactory factory) {
@@ -18,5 +23,12 @@ public class ReactiveMongoDBTransactionConfig {
     @Bean
     public TransactionalOperator transactionalOperator(ReactiveTransactionManager txm) {
         return TransactionalOperator.create(txm);
+    }
+
+    @Bean
+    public MongoClientSettingsBuilderCustomizer mongoClientSettingsBuilderCustomizer(MeterRegistry meterRegistry) {
+        return builder -> builder.writeConcern(WriteConcern.ACKNOWLEDGED)
+                .applyToConnectionPoolSettings(builder1 -> builder1.minSize(5));
+
     }
 }
