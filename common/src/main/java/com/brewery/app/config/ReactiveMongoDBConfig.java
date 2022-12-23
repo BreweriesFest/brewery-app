cfca006ef2138e6c4f0ec25c874b6f08013333ff
Shubham Goel <shubhamgoyal23@yahoo.com>
2022-12-23 10:22:31 +0530

    enable mongo configuration based on profile, disable redis auto-configure

--- common/src/main/java/com/brewery/app/config/ReactiveMongoDBConfig.java
+++ common/src/main/java/com/brewery/app/config/ReactiveMongoDBConfig.java
@@ -5,13 +5,15 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
 import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.reactive.TransactionalOperator;
 
 @Configuration
-public class ReactiveMongoDBTransactionConfig {
+@Profile("mongo-db")
+public class ReactiveMongoDBConfig {
 
     @Bean
     public ReactiveTransactionManager transactionManager(ReactiveMongoDatabaseFactory factory) {
