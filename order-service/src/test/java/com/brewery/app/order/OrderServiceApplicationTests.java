0768a5aa097c3c6657cb163bf68243ef7853f399
Shubham Goel <shubhamgoyal23@yahoo.com>
2022-12-30 11:39:36 +0530

    Refactor (#40)
    * add testContainers
    
    * update java to 17
    
    * add devcontainer json
    
    * update git workflow for monorepo build
    
    * update git workflow for monorepo build
    
    * update git workflow for monorepo build
    
    * update git workflow for monorepo build
    
    * update git workflow for monorepo build
    
    * update git workflow for monorepo build
    
    * update git workflow for monorepo build
    
    * create reusable workflow
    
    * create reusable workflow
    
    * create reusable workflow
    
    * create reusable workflow
    
    * create reusable workflow
    
    * create reusable workflow
    
    * create reusable workflow
    
    * create reusable workflow
    
    * create reusable workflow
    
    * update reactive consumer

--- order-service/src/test/java/com/brewery/app/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/brewery/app/order/OrderServiceApplicationTests.java
@@ -1,23 +1,35 @@
-package com.brewery.app.scheduler;
+package com.brewery.app.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
-class SchedulerApplicationTests {
+class OrderServiceApplicationTests {
 
     @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+    @Container
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:latest"))
+            .withExposedPorts(6379);
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+        registry.add("spring.redis.host", redis::getHost);
+        System.setProperty("spring.redis.port", redis.getMappedPort(6379).toString());
     }
 
     @Test
