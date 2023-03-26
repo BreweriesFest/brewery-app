9b39ee9896351661bd17bec5884c7f420919d688
Shubham Goel <shubhamgoyal23@yahoo.com>
2023-03-26 23:42:45 +0530

    custom metrics

--- common/src/main/java/com/brewery/common/client/GraphqlClient.java
+++ common/src/main/java/com/brewery/common/client/GraphqlClient.java
@@ -55,36 +55,26 @@ public abstract class GraphqlClient {
 			.tag("client", resilienceId)
 			.tag(TENANT_ID, Helper.fetchHeaderFromContext.apply(TENANT_ID, ctx))
 			.tag(CUSTOMER_ID, Helper.fetchHeaderFromContext.apply(CUSTOMER_ID, ctx))
-			.tap(Micrometer.observation(registry)));
-	}
-
-	protected <T> Mono<T> fromMono(Collection<String> headers, String document, Map<String, Object> variables,
-			ParameterizedTypeReference<T> entityType) {
-		return getHttpGraphQlClient(headers)
-			.flatMap(__ -> __.document(document).variables(variables).retrieve("data").toEntity(entityType))
-			.transform(it -> {
+			.tap(Micrometer.observation(registry))).transform(it -> {
 				ReactiveCircuitBreaker rcb = reactiveCircuitBreakerFactory.create(resilienceId);
 				return rcb.run(it, throwable -> {
 					log.error("exception::{}", throwable.getMessage(), throwable);
 					return Mono.error(new BusinessException(ExceptionReason.UPSTREAM_SERVER_ERROR));
 				});
-			})
-			.transformDeferred(RetryOperator.of(retry));
+			}).transformDeferred(RetryOperator.of(retry));
+	}
+
+	protected <T> Mono<T> fromMono(Collection<String> headers, String document, Map<String, Object> variables,
+			ParameterizedTypeReference<T> entityType) {
+		return getHttpGraphQlClient(headers)
+			.flatMap(__ -> __.document(document).variables(variables).retrieve("data").toEntity(entityType));
 	}
 
 	protected <T> Flux<T> fromFlux(Collection<String> headers, String document, Map<String, Object> variables,
 			ParameterizedTypeReference<T> entityType) {
 		return getHttpGraphQlClient(headers)
 			.flatMap(__ -> __.document(document).variables(variables).retrieve("data").toEntityList(entityType))
-			.flatMapMany(Flux::fromIterable)
-			.transform(it -> {
-				ReactiveCircuitBreaker rcb = reactiveCircuitBreakerFactory.create(resilienceId);
-				return rcb.run(it, throwable -> {
-					log.error("exception::{}", throwable.getMessage(), throwable);
-					return Flux.error(new BusinessException(ExceptionReason.UPSTREAM_SERVER_ERROR));
-				});
-			})
-			.transformDeferred(RetryOperator.of(retry));
+			.flatMapMany(Flux::fromIterable);
 
 	}
 
